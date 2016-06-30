b520d1f33ce16d27a45c087c9413b7e29808d95c
Tyjow <platon.jonathan@gmail.com>
2016-06-30 14:34:09 +0200

    insertion de multiplicateur

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+var compteur = 1;
+var score = 0;
+
 function affichage() {
-    var score = document.getElementById('affichage').innerHTML;
-    score++;
+    score = score + compteur;
     document.getElementById('affichage').innerHTML = score;
+}
+
+function incrementer() {
+	if (score >= 20*compteur*compteur) {
+		compteur = compteur*2;
+		score = score - 50;
+		document.getElementById('multiplicateur').innerHTML = "multiplicateur x" + compteur;
+		document.getElementById('palier').innerHTML = "Prix du prochain : " + 20*compteur*compteur;
+	}
+	else {
+		alert("Tu ne peux pas encore acheter !");
+	}
 }
